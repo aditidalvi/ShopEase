99ed17d2da40fea684bd21be8ffffe09e67aace1
aditidalvi <aditidalvi22@gmail.com>
2025-02-23 11:32:25 +0530

    Initial Commit

---
+++ ShopEase/ShopEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopEaseApp.swift
+//  ShopEase
+//
+//  Created by Aditi Dalvi on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
